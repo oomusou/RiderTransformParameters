8b06b2b3ea09ea61f39f709b38432b86bd826bc8
oomusou <oomusou@gmail.com>
2018-07-08 11:04:34 +0800

    一天小費 10 元 (重構測試)

--- ServiceLib.UnitTest/UnitTest1.cs
+++ ServiceLib.UnitTest/UnitTest1.cs
@@ -6,14 +6,24 @@ namespace ServiceLib.UnitTest
     [TestClass]
     public class UnitTest1
     {
+        private StateroomService _stateroomService;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _stateroomService = new StateroomService();
+        }
+
         [TestMethod]
         public void 一天小費10元()
         {
-            var stateroomService = new StateroomService();
-            var startTime = new DateTime(2018, 7, 7);
-            var endTime = new DateTime(2018, 7, 7);
-            var totalAmount = stateroomService.TotalAmount(startTime, endTime);
-            Assert.AreEqual(10m, totalAmount);
+            AmountShouldBe(10m, new DateTime(2018, 7, 7), new DateTime(2018, 7, 7));
+        }
+
+        private void AmountShouldBe(decimal expected, DateTime startTime, DateTime endTime)
+        {
+            var totalAmount = _stateroomService.TotalAmount(startTime, endTime);
+            Assert.AreEqual(expected, totalAmount);
         }
     }
 }
