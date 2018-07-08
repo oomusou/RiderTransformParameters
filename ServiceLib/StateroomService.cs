3dc1d001146d98920598117124781b888d7fd42e
oomusou <oomusou@gmail.com>
2018-07-08 10:48:58 +0800

    一天小費 10 元 (綠燈)

---
+++ ServiceLib/StateroomService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ServiceLib
+{
+    public class StateroomService
+    {
+        public decimal TotalAmount(DateTime startTime, DateTime endTime)
+        {
+            return 10m;
+        }
+    }
+}
